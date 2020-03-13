ea0f777ebe7fbdcb7624832738d18e134900b4c3
Jiyeon <mycasio1@naver.com>
2020-03-14 04:48:01 +0900

    views , models 수정

--- app/models.py
+++ app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Date_Convert(models.Model):
+    standard_date = models.datetimefield(default=timezone.now)
+
+class Unit_Convert(models.Model):
+    standard_length = models.IntegerField(default=0)
+    standard_weight = models.IntegerField(default=0)
+    standard_volume = models.IntegerField(default=0)
--- app/views.py
+++ app/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Date_Convert
+from .models import Unit_Convert
+
 
 # Create your views here.
 
@@ -6,17 +10,40 @@ def home(request):
     return render(request,'home.html')
 
 def date(request):
-    return render(request,'date.html')
+    if request.method == 'POST' :
+        Date_Convert = Date_Convert()
+        Date_Convert.standard_date = request.POST['standard_date']
+        Date_Convert.save()
+        return render(request,'date.html')
+    else:
+        return render(request,'date.html')
 
 def unit(request):
     return render(request,'unit.html')
 
 def length(request):
-    return render(request, 'length.html')
+    if request.method == 'POST' :
+        Unit_Convert = Unit_Convert()
+        Unit_Convert.standard_length = request.POST['standard_length']
+        Unit_Convert.save()
+        return render(request,'length.html')
+    else:
+        return render(request,'length.html')
 
 def weight(request):
-    return render(request, 'weight.html')
+    if request.method == 'POST' :
+        Unit_Convert = Unit_Convert()
+        Unit_Convert.standard_weight = request.POST['standard_weight']
+        Unit_Convert.save()
+        return render(request,'weight.html')
+    else:
+        return render(request,'weight.html')
 
 def volume(request):
-    return render(request, 'volume.html')
-
+    if request.method == 'POST' :
+        Unit_Convert = Unit_Convert()
+        Unit_Convert.standard_volume = request.POST['standard_volume']
+        Unit_Convert.save()
+        return render(request,'volume.html')
+    else:
+        return render(request,'volume.html')
