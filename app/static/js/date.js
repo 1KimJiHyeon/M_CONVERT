997390c13d59bb6de8218c861d668abb90d9c278
Han-Joon-Hyeok <virq@naver.com>
2020-03-13 15:36:01 +0900

    add AdToBuddist Year in 'date.html' with js

---
+++ app/static/js/date.js
@@ -0,0 +1,4 @@
+/* When the input field receives input, convert the value from feet to meters */
+function AfterDieToBuddhist(valNum) {
+    document.getElementById("outputMeters").innerHTML = parseInt(valNum) + 544 ;
+  }
