116911973b2aee8b1ef5625179bc0e94d4e69297
Han-Joon-Hyeok <virq@naver.com>
2020-03-14 22:30:22 +0900

    add onclick attribute in input box

--- app/static/js/date.js
+++ app/static/js/date.js
@@ -1,4 +1,23 @@
 /* When the input field receives input, convert the value from feet to meters */
 function AfterDieToBuddhist(valNum) {
-    document.getElementById("outputMeters").innerHTML = parseInt(valNum) + 544 ;
+    document.getElementById("OutputBuddhist").innerHTML = parseInt(valNum) + 544 ;
   }
+
+function AfterDieToDanki(valNum) {
+  document.getElementById("OutputDanki").innerHTML = parseInt(valNum) + 2333 ;
+  }
+
+
+function SetCurrentYear(){
+  var d = new Date();
+  var n = d.getFullYear();
+  document.getElementById('InputAD').value = n;
+
+  // document.getElementById('CurrentYear').valueAsDate = new Date();
+  // var today = new Date();	
+  // $("#CurrentYear").val(getYYYY(today));
+  // document.getElementById('CurrnetYear').value= new Date().toISOString().slice(0, 7);
+  
+  // document.getElementById('CurrnetYear').valueAsDate = new Date();
+
+}
