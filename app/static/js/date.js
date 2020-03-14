22cab3310d38e26654e6d5ca6e95020eb176a3e3
Han-Joon-Hyeok <virq@naver.com>
2020-03-14 20:54:30 +0900

    .

--- app/static/js/date.js
+++ app/static/js/date.js
@@ -1,17 +1,4 @@
-<<<<<<< HEAD
-var temp;
-
-function input(){
-var input = document.getElementById("input").value;
-temp = input;
-document.getElementById("output").value = temp;
-}
-
-
-
-=======
 /* When the input field receives input, convert the value from feet to meters */
 function AfterDieToBuddhist(valNum) {
     document.getElementById("outputMeters").innerHTML = parseInt(valNum) + 544 ;
   }
->>>>>>> 997390c13d59bb6de8218c861d668abb90d9c278
